84183576b6065befdb4e7ae6ce31988dfd6427fd
unknown <miroslavadmiral@gmail.com>
2019-03-14 19:52:09 +0300

    Handler Button Sender

--- WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/Form1.Designer.vb
@@ -126,7 +126,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(308, 300)
+        Me.ClientSize = New System.Drawing.Size(295, 300)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.IncomingMessage)
         Me.Controls.Add(Me.StartStopKnopka)
--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,9 +1,24 @@
 ﻿Public Class Form1
+    Dim Client As New UNOLibs.Net.ClientClass
+    Dim WithEvents Server As UNOLibs.Net.ServerClass
+
     Private Sub NumericUpDown2_ValueChanged(sender As Object, e As EventArgs) Handles ServerPort.ValueChanged
 
     End Sub
 
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub KnopkaOtpravkiMsg_Click(sender As Object, e As EventArgs) Handles KnopkaOtpravkiMsg.Click
+        Dim IP As String = IPDest.Text
+        Dim PORT As Integer = PortDest.Value
+        Dim DATA As String = IshodMessage.Text
+        Try
+            Client.SendMessage(IP, PORT, DATA)
+        Catch ex As Exception
+            MsgBox("Error sending message")
+        End Try
 
     End Sub
 End Class
